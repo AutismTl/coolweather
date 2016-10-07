857ae8b9c1e92def975825c6ff4e65ce0184b2a0
TL <840209925@qq.com>
2016-10-07 22:39:14 +0800

    除后台外，完完成.

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -3,9 +3,13 @@ package com.coolweather.app.activity;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.R.bool;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -51,9 +55,20 @@ public class ChooseAreaActivity extends Activity {
 	//当前选中的级别
 	private int currentLevel;
 	
+	//是否从WeatherActivity跳转过来
+	private boolean isFromWeatherActivity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity=getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)&& !isFromWeatherActivity){
+			Intent intent =new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView=(ListView)findViewById(R.id.list_view);
@@ -70,6 +85,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if(currentLevel==LEVEL_CITY){
 					selectedCity=cityList.get(index);
 					queryCounties();
+				}else if(currentLevel==LEVEL_COUNTY){
+					String countyCode=countyList.get(index).getCountyCode();
+					Intent intent =new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
@@ -220,6 +241,10 @@ public class ChooseAreaActivity extends Activity {
 			}else if(currentLevel==LEVEL_CITY){
 				queryProvinces();
 			}else{
+				if(isFromWeatherActivity){
+					Intent intent=new Intent(this,WeatherActivity.class);
+					startActivity(intent);
+				}
 				finish();
 			}
 		}
--- src/com/coolweather/app/util/HttpUtil.java
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class HttpUtil {
 	
 	public static void sendHttpRequest(final String address,final HttpCallbackListener listener){
@@ -17,10 +18,12 @@ public class HttpUtil {
 					URL url=new URL(address);
 					connection=(HttpURLConnection)url.openConnection();
 					connection.setRequestMethod("GET");
+					connection.setRequestProperty("apikey", "3c68b29f100dc97a6d67f2096473b358");
+			        connection.connect();
 					connection.setConnectTimeout(8000);
 					connection.setReadTimeout(8000);
 					InputStream in=connection.getInputStream();
-					BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+					BufferedReader reader=new BufferedReader(new InputStreamReader(in,"UTF-8"));
 					StringBuilder response=new StringBuilder();
 					String line;
 					while((line=reader.readLine())!=null){
@@ -33,6 +36,7 @@ public class HttpUtil {
 				}catch(Exception e){
 					if(listener!=null){
 						//回调onError()方法
+						e.printStackTrace();
 						listener.onError(e);
 					}
 					}finally{
@@ -43,4 +47,5 @@ public class HttpUtil {
 			}
 		}).start();
 	}
+	
 }
